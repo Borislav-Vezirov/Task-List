50a69c3ce2fa2527da5ec91db2a3df939703a5c4
Borislav-Vezirov <59201022+Borislav-Vezirov@users.noreply.github.com>
2020-06-03 21:16:38 +0300

    Add files via upload

---
+++ Task-List/main.js
@@ -0,0 +1,177 @@
+let form        = document.querySelector('.btn-info');
+let input       = document.querySelector('#task');
+let filter      = document.querySelector('#filter');
+let ul          = document.querySelector('.list-group');
+let clearTasks  = document.querySelector('.btn-dark');
+
+form       .addEventListener('click', addTask);
+ul         .addEventListener('click', removeTask);
+clearTasks .addEventListener('click', clearAllTasks);
+filter     .addEventListener('keyup', filterTasks);
+
+document   .addEventListener('DOMContentLoaded', getTasks);
+
+function getTasks() {
+    
+    let tasks
+
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }  else {
+        tasks = JSON.parse(localStorage.getItem('task'));
+    }
+
+    tasks.forEach(function(task) {
+
+        let li         = document.createElement('li');
+
+        li.className   = 'list-group-item';
+        
+        li.appendChild(document.createTextNode(task));
+
+        let link       = document.createElement('a');
+
+        link.className = 'link';
+
+        link.innerHTML = '<i class="fas fa-trash-alt float-right"></i>';
+
+        li.appendChild(link);
+
+        ul.appendChild(li);
+        
+    });
+}
+function addTask(e) {
+
+    e.preventDefault();
+
+    if(input.value === "") {
+        alert('You must write something..');
+    } else {
+      
+        let li         = document.createElement('li');
+
+        li.className   = 'list-group-item';
+        
+        li.appendChild(document.createTextNode(input.value));
+
+        let link       = document.createElement('a');
+
+        link.className = 'link';
+
+        link.innerHTML = '<i class="fas fa-trash-alt float-right"></i>';
+
+        li.appendChild(link);
+
+        ul.appendChild(li);
+
+        storeTaskInLocalStorage(input.value);
+        
+        input.value    = "";
+    }
+
+}
+
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+  
+    tasks.push(task);
+  
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  
+
+// function filterTasks() {
+
+//     var text = filter.value.toLowerCase();
+//     var items = document.getElementsByClassName('list-group-item');
+
+
+//     for(var i = 0; i < items.length; i++) {
+//         text = items[i].textContent || items[i].innerHTML;
+//         if(items[i].toLowerCase().indexOf(text) != -1) {
+//             items[i].style.display = "block";
+//         } else {
+//             items[i].style.display = "none"
+//         } 
+//     }
+// }
+
+function filterTasks(e) {
+    let text = e.target.value.toLowerCase();
+  
+    document.querySelectorAll('.list-group-item').forEach(function(task){
+      let item = task.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+      } else {
+        task.style.display = 'none';
+      }
+    });
+  }
+
+function removeTask(e) {
+
+    if(e.target.parentElement.classList.contains('link')) {
+        if(confirm('Are you sure you want to delete the task?')) {
+            e.target.parentElement.parentElement.remove();
+
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+        }
+    }
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+  
+    tasks.forEach(function(task, index){
+      if(taskItem.textContent === task){
+        tasks.splice(index, 1);
+      }
+    });
+  
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function clearAllTasks() {
+
+    if(confirm('Are you sure you want to delete all tasks?')) {
+
+        ul.textContent = '';
+
+        clearTasksFromLocalStorage();
+
+    }
+}
+
+function clearTasksFromLocalStorage() {
+
+    localStorage.clear();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
